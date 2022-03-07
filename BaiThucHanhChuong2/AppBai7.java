f2c502064b210a8233fa5bd015064ed67e2decef
tranhanhh <97442873+tranhanhh@users.noreply.github.com>
2022-03-08 00:31:58 +0700

    Add files via upload

---
+++ BaiThucHanhChuong2/AppBai7.java
@@ -0,0 +1,9 @@
+package BaiThucHanhChuong2;
+public class AppBai7 {
+    public static void main(String[] args)  {
+        System.out.println("20 so nguyen duong dau tien la:");
+        for(int i=0; i<=20; i++){
+            System.out.println(i);
+        }
+    }
+}
